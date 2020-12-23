e583e9e191990b0c1a801cca1a9e602d1fb4e938
ineserdoura <ineserdoura@hotmail.com>
2020-12-23 23:53:07 +0000

    changed random variable

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const words = [
   "bicicle",
   "titanic",
   "football",
-  "Thailand"
+  "Thailand",
 ];
 const clues = [
   "Capital of the Netherlands",
@@ -14,12 +14,13 @@ const clues = [
   "It only have two wheels, pedals and a handlebar",
   "Luxury British steamship that sank after striking an iceberg",
   "In the US they call it soccer, but in Europe we call it...",
-  "The Land of Smiles."
+  "The Land of Smiles.",
 ];
 
 // choose word from array;
 
-const random = Math.floor(Math.random() * words.length + 1);
+const random = Math.floor(Math.random() * words.length);
+console.log(random)
 const chosenWord = words[random];
 const chosenClue = clues[random];
 
@@ -45,24 +46,17 @@ function createUnderscores() {
   return underscore.join(" ");
 }
 cWord.innerHTML = `${createUnderscores()}`;
-// fill _ with letters
-let rightWord = [];
-
 //  get user input and play
 let gLeft = ["❤️", "❤️", "❤️", "❤️", "❤️", "❤️"];
 lives.innerHTML = `${gLeft.join("")}`;
 
 button.addEventListener("click", () => {
-  
   let input = userInput.value.trim().toLowerCase();
-  if (
-    
-    (input.charCodeAt(0)<97 || input.charCodeAt(0)>122)
-  ) {
+  if (input.charCodeAt(0) < 97 || input.charCodeAt(0) > 122) {
     lives.innerHTML = `Please insert a letter!`;
     return;
   }
-  
+
   let indices = [];
   for (let i = 0; i < chosenWord.length; i++) {
     if (chosenWord[i] === input) {
@@ -70,7 +64,6 @@ button.addEventListener("click", () => {
     }
   }
   if (chosenWord.includes(input)) {
-  
     for (let i = 0; i < indices.length; i++) {
       underscore[indices[i]] = input;
     }
@@ -82,7 +75,6 @@ button.addEventListener("click", () => {
     wrongLetters.innerHTML += `${input} `;
     gLeft.pop();
     lives.innerHTML = `${gLeft.join("")}`;
-    
   }
   if (gLeft.length == 0) {
     lives.innerHTML = `GAME OVER. <br> The word was ${chosenWord}. Try again`;
